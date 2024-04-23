0847e45f5855a50ea16591eda4183df5f050a1a1
merefield <merefield@gmail.com>
2024-04-23 07:19:00 +0100

    really working now

--- javascripts/discourse/components/home-page-layout.js
+++ javascripts/discourse/components/home-page-layout.js
@@ -5,25 +5,6 @@ import { tracked } from "@glimmer/tracking";
 
 export default class HomePageLayoutComponent extends Component {
   @service router;
-  // @service site;
-  // @tracked page;
-  // @tracked toggleState = "expanded";
-  // @tracked visability = "show";
-
-  // constructor() {
-  //   super(...arguments);
-  //   let page = JSON.parse(settings.homepage_components).filter(page => page.pageName === this.page_name)[0];
-  //   page.lines.forEach((line) => {
-  //     line.components.forEach((component) => {
-  //       if (getOwner(this).hasRegistration(`component:${component.component_name}`)) {
-  //         component.parsedParams = this.parseParams(component.params);
-  //       } else {
-  //         console.log(`Component ${component.component_name} not found`);
-  //       }
-  //     });
-  //   });
-  //   this.page = page;
-  // }
 
   parseParams(params) {
     let parsedParams = {}
@@ -57,18 +38,4 @@ export default class HomePageLayoutComponent extends Component {
     });
     return page;
   };
-
-  <template>
-    <div class="{{this.page.className}} homepage-content">
-      {{#each this.page.lines as |line|}}
-          <div class="{{line.className}} homepage-line">
-            {{#each line.components as |c|}}
-              <div class="component-widget">
-                {{component c.component_name params=c.parsedParams}}
-              </div>
-            {{/each}}
-          </div>
-      {{/each}}
-    </div>
-  </template>
 }
